14c08e87a17641de8c5162ae24f95c5e9d98ffef
degurrrrrr <degurrrrrr@gmail.com>
2022-05-13 18:46:04 +0900

    DropDown 구현 완료

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -7,8 +7,11 @@ import SearchIcon from '@mui/icons-material/Search';
 const DropDown = () => {
   const [isOpen, setIsOpen] = react.useState(false);
   const [clickedMenu, setClickedMenu] = react.useState('전체');
+  const [inputText, setInputText] = react.useState('');
 
-  const myList = ['전체', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'];
+  console.log(inputText);
+
+  const myList = ['All Symbols', 'BTCUSD.PERP', 'ETHUSD.PERP', 'BCHUSD.PERP', 'LTCUSD.PERP', 'BANDUSD.PERP', 'XRPUSD.PERP'];
 
   const clickOpen = () => {
     setIsOpen(!isOpen);
@@ -16,31 +19,46 @@ const DropDown = () => {
 
   return (
     <div style={{ marginTop: '50px' }}>
-      <div onClick={clickOpen} className="dropdown" style={{ position: 'relative', marginBottom: '10px' }}>
-        {clickedMenu}
+      <div onClick={clickOpen} className="dropdown">
+        <div style={{ paddingLeft: '10px' }}>{clickedMenu}</div>
         <ArrowDropDownIcon style={{ position: 'absolute', right: '10px' }} />
       </div>
       {isOpen ? (
-        <>
-          <Result>
-            {/* <div style={{ position: 'relative' }}>
-              <Search type="text" />
-              <SearchIcon style={{ position: 'absolute', top: '10px' }} />
-            </div> */}
-            {myList.map((e, i) => (
-              <div
-                onClick={() => {
-                  setClickedMenu(e);
-                  setIsOpen(!isOpen);
-                }}
-                key={i}
-                style={{ cursor: 'pointer' }}
-              >
-                {e}
-              </div>
-            ))}
-          </Result>
-        </>
+        <Container>
+          <SearchWrap>
+            <SearchIcon style={{ width: '18px', color: '#ddd' }} />
+            <SearchInput
+              onChange={(e) => {
+                setInputText(e.target.value);
+              }}
+              type="text"
+              placeholder="검색어를 입력하세요"
+              autoFocus
+            />
+          </SearchWrap>
+          <DropdownWrap>
+            {myList
+              .filter((val, i) => {
+                if (i === 0 || inputText == '') {
+                  return val;
+                } else if (val.toLowerCase().includes(inputText.toLowerCase())) {
+                  return val;
+                }
+              })
+              .map((p, i) => (
+                <Menu
+                  onClick={() => {
+                    setClickedMenu(p);
+                    setIsOpen(!isOpen);
+                  }}
+                  key={i}
+                  length={myList.length}
+                >
+                  {p}
+                </Menu>
+              ))}
+          </DropdownWrap>
+        </Container>
       ) : (
         ''
       )}
@@ -48,34 +66,59 @@ const DropDown = () => {
   );
 };
 
-const Search = styled.input`
-  width: 100%;
-  height: 35px;
-  background-color: #ada;
+const Container = styled.div`
+  width: 280px;
+  height: 100%;
 
+  border: 1px solid #bbb;
+  border-radius: 5px;
+
+  box-shadow: 0px 2px 4px #ddd;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const SearchWrap = styled.div`
+  height: 40px;
+  border-bottom: 1px solid #bbb;
+  display: flex;
+  align-items: center;
+
+  padding-left: 10px;
+`;
+
+const SearchInput = styled.input`
+  margin-left: 8px;
   border: none;
+  color: #a4a4a4;
+  font-size: 16px;
 
   :focus {
     outline: none;
   }
-
-  display: flex;
-  align-items: center;
+  ::placeholder {
+    color: #ddd;
+  }
 `;
 
-const Result = styled.div`
+const DropdownWrap = styled.div`
   width: 280px;
-  height: 280px;
-  border: 1px solid #bbb;
-  border-radius: 5px;
-  /* border-bottom-right-radius: 5px;
-  border-bottom-left-radius: 5px; */
-
-  padding: 5px 0px 5px 20px;
 
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+`;
+
+const Menu = styled.div`
+  height: 35px;
+  padding-left: 25px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+
+  :hover {
+    background-color: #eee;
+  }
 `;
 
 export default DropDown;
